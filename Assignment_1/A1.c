32da5fff79599dca09d81c8ffbfe3cafa5b63c5c
Naina-rc2k1 <95739423+Naina-rc2k1@users.noreply.github.com>
2023-10-09 12:59:34 +0530

    Add files via upload

---
+++ Assignment_1/A1.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+int main()
+{
+   /*Q1*/
+   printf("/***Q1***/ \n");
+   printf("Hello") ;
+   printf("\n\n");
+
+   /*Q2*/
+   printf("/***Q2***/\n") ;
+   printf("Hello\nStudents\n\n");
+
+   /*Q3*/
+   printf("/***Q3***/\n") ;
+   printf("\"MySirG\"\n\n");
+
+   /*Q4*/
+   printf("/***Q4***/\n") ;
+   int  r;
+   float area;
+   printf("Enter the radius");
+   scanf("%d",&r);
+   area=r*r;
+   printf("Area of circle is %f having the radius %d \n\n",area,r);
+
+
+   /*Q5*/
+   printf("/***Q5***/\n") ;
+   int x=printf("naina\n");
+   printf("length of string is %d \n\n",x);
+
+
+   /*Q6*/
+   printf("/***Q6***/\n") ;
+   printf("\"Naina\"\n\n");
+
+
+
+   /*Q7*/
+   printf("/***Q7***/\n") ;
+   printf("\"%%d\"\n\n");
+
+   /*Q8*/
+   printf("/***Q8***/\n") ;
+   printf("\"\\n\"\n\n");
+
+   /*Q9*/
+   printf("/***Q9***/\n") ;
+   printf("\"\\\\\"\n\n");
+
+   /*Q10*/
+   printf("/***Q10***/\n") ;
+   int d,m,y;
+   printf("Enter Date in DD/MM/YYYY FORMAT");
+   scanf("%d/%d/%d",&d,&m,&y);
+   printf("Day -%d ,Month -%d ,Year -%d \n\n",d,m,y);
+
+   /*Q11*/
+   printf("/***Q11***/\n") ;
+   int h,min;
+   printf("Enter the Time in HH:MM Format:");
+   scanf("%d:%d",&h,&min);
+   printf("%d Hour and %d Minute \n\n",h,min);
+
+    /*Q12*/
+   printf("/***Q12***/\n") ;
+   printf("OUTPUT IS: ineuron7\n\n");
+
+}
